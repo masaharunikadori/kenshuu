1a797f2288b66d71dd835a1bba938030b687f71b
masaharunikadori <masaharu.nikadori@tafs.co.jp>
2015-12-17 14:32:23 +0900

    aa

--- Flowchar1.java
+++ Flowchar1.java
@@ -1,9 +1,30 @@
-package jp.co.tafs.kenshu;
+package src.main.java;
 
 public class Flowchar1 {
 
 	public static void main(String[] args) {
-		
+		kadai1();
 	}
-	
+
+	public static void kadai1() {
+
+		for (int k = 1; k <= 100; k++) {
+
+			if (k % 15 == 0) {
+				System.out.println("Fizzbazz");
+			}
+			else if (k % 3 == 0) {
+				System.out.println("Fizz");
+			}
+			else if (k % 5 == 0) {
+				System.out.println("bazz");
+			}
+			else {
+				System.out.println(k);
+			}
+
+		}
+
+	}
+
 }
